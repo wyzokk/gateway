2e9ad25c85683acd5d46a9f253f48db5191e5a9d
wyzokk <wyzokk@hotmail.com>
2021-04-13 09:56:26 +0800

    Delete Constant.java

--- Constant.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.sdas.gateway.contants;
-
-public class Constant {
-
-	public static final String LOGGER_NAME = "com.cloud.sdas.gateway.logger";
-	
-	public static final String MOCK_YES ="YES";
-	
-}
